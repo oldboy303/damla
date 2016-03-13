bc3afe34a42651e965e9be0cfb076f89e18c4972
San Guyer <ibo303@me.com>
2016-03-13 13:14:05 -0600

    email localStorage check added

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ $(function() {
       $('#name').focus();
     } else if ((!emailRegex.test(email) || email.length === 0)) {
       $('#email').focus();
-    }else if (localStorage.getItem(name)) {
+    }else if (localStorage.getItem(name) && (JSON.parse(localStorage.getItem(name)).email === email)) {
       currentPlayer = JSON.parse(localStorage.getItem(name));
       $('#theme').prepend('welcome back ' + name + '! <br> please pick a theme:');
       $(this).hide('slow');
